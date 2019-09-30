fc6a2623701cd61e24fc9ed67fdf24ae9b0b9792
Chris Flick <chrisflick@gmail.com>
2019-09-30 13:50:22 -0700

    Page now properly displays name of pokemom, pokedex number, and sprite

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,3 +1,31 @@
+$(document).ready(function() {
+    let pokemon = getPokemon('bulbasaur');
+
+    pokemon.then(function(response) {
+        console.log(response)
+
+        $('#pokemon').empty()
+
+        let $sprite = $('<img>')
+        let $name = $('#name')
+        let $height = $('#height')
+        
+        $name.text(response.name + " #" + response.id)
+        $height.text('0.' + response.height);
+
+        $sprite.attr('src', response.sprites.front_default)
+
+        $('#sprite').append($sprite)    
+
+
+    })
+})
+
+
+/*********************
+****** Functions *****
+*********************/
+
 function getPokemon(mon) {
     let queryURL = `https://pokeapi.co/api/v2/pokemon/${mon}`
 
